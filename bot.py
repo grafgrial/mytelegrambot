1d706bfeb7fd23125e56349f28756ee2e3251924
Grafgrial <47384495+grafgrial@users.noreply.github.com>
2025-03-14 11:30:56 +0500

    test1

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 def start(bot, update):
     bot.send_message(chat_id=update.effective_chat.id, text="Welcome! \nTo get started, you can choose one of the following options: \n/help - To learn how to use this bot \n/buy - To buy access and start using our services \n/settings - To manage your account settings")
-Create a function to handle the /buy command:
 
 def buy(bot, update):
     bot.send_message(chat_id=update.effective_chat.id, text="Thanks for choosing our services! \nTo get started, you can choose one of the following options: \n-Probationary period (7 days) \n-Pay for access and start using our services")
@@ -14,7 +13,6 @@ def buy(bot, update):
     markup.add(item1, item2)
 
     bot.send_message(chat_id=update.effective_chat.id, text="Please select an option:", reply_markup=markup)
-Create a function to handle the /help command:
 
 def help(bot, update):
-    bot.send_message(chat_id=update.effective_chat.id, text="This bot provides the following commands: \n/start - To
+    bot.send_message(chat_id=update.effective_chat.id, text="This bot provides the following commands: \n/start - To start\n/buy - To buy access and start using our services\n/settings - To manage your account settings")
