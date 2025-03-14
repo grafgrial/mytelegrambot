0cc163add73e932cead5b44ad0b64eafb828d487
Grafgrial <47384495+grafgrial@users.noreply.github.com>
2025-03-14 17:35:45 +0500

    test13

--- info.py
+++ info.py
@@ -2,5 +2,4 @@ import os
 from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
-''''BOT_TOKEN = environ.get('API_TOKEN', "")'''
-BOT_TOKEN = os.getevn("API_TOKEN")
+BOT_TOKEN = os.getenv("API_TOKEN")
