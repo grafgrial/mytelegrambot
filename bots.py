7cb70bcdcdc279855bfffb7c9e4ef133b5ee0057
Grafgrial <47384495+grafgrial@users.noreply.github.com>
2025-06-24 16:18:42 +0500

    1012

---
+++ bots.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+from telegram import Update, Bot
+
+app = Flask(__name__)
+
+# Замените на свой токен
+bot = os.getenv("API_TOKEN") # Вставьте сюда токен вашего бота
+
+@app.route(f'/{TOKEN}', methods=['POST'])
+def webhook():
+    update = Update.de_json(request.get_json(force=True), bot)
+    chat_id = update.message.chat_id
+    bot.send_message(chat_id=chat_id, text="Привет от бота на Render!")
+    return 'OK'
+
+@app.route('/')
+def index():
+    return 'Bot is running!'
+
+if __name__ == '__main__':
+    app.run()
